3be8b307d6b3b03f2a0fafbc0951796ac902b560
mjkim134 <mjkim134@naver.com>
2026-01-14 23:49:32 +0900

    wip: make vpc with terraform

--- terraform/vpc/backend.tf
+++ terraform/vpc/backend.tf
@@ -1,9 +1,9 @@
-# backend configuration with locking
+# backend configuration
 
 terraform {
   backend "s3" {
     bucket = "mjkim134-tfstate-bucket"
-    key    = "terraform/init/terraform.tfstate"
+    key    = "terraform/vpc/terraform.tfstate"
     region = "ap-northeast-2"
 
     use_lockfile = true
